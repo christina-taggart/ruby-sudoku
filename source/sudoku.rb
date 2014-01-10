f972ae5a9d10d94863fa8c828ce05634e04d2047
Darcey Lachtman & Emily Foley <pair+dslachtman.emfoley@devbootcamp.com>
2014-01-10 12:31:38 -0800

    added running mvp for checking rows

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,9 +1,19 @@
 class Sudoku
-  def initialize(board_string)
+  def initialize(given,given_row)
+    @given = given
+    @given_row = [0,0,3,5,0,1,0,9,4]
+    @needed = [1,2,3,4,5,6,7,8,9]
   end
 
-  def solve!
-  end
+  #def solve!
+    def check_row(given_row)
+     checking = given_row.clone.delete_if {|element| element == 0}
+     @needed -= checking
+
+     p @needed
+   end
+
+  #end
 
   # Returns a string representing the current state of the board
   # Don't spend too much time on this method; flag someone from staff
@@ -15,11 +25,13 @@ end
 
 # The file has newlines at the end of each line, so we call
 # String#chomp to remove them.
-board_string = File.readlines('sample.unsolved.txt').first.chomp
-
-game = Sudoku.new(board_string)
+given = File.readlines('sample.unsolved.txt').first.chomp
+ given_row = [0,0,3,5,0,1,0,9,4]
+    needed = [1,2,3,4,5,6,7,8,9]
+game = Sudoku.new(given,given_row)
 
 # Remember: this will just fill out what it can and not "guess"
-game.solve!
+# game.solve!
 
-puts game.board
+# puts game.board
+game.check_row(given_row)
