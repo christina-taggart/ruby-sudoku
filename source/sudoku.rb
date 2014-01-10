6908561ff98c0e1881c7a7b6dd994cbc56f800d2
Darcey Lachtman & Emily Foley <pair+dslachtman.emfoley@devbootcamp.com>
2014-01-10 14:55:31 -0800

    created rows method, now have unsolved board

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,16 +1,26 @@
 class Sudoku
   def initialize(given,given_row)
     @given = given
-    @given_row = [0,0,3,5,0,1,0,9,4]
     @needed = [1,2,3,4,5,6,7,8,9]
+    @rows = create_rows
   end
 
   #def solve!
+  #complete is 1..9
+  #needed starts at 1..9
     def check_row(given_row)
      checking = given_row.clone.delete_if {|element| element == 0}
      @needed -= checking
-
-     p @needed
+   end
+#create a nested array of rows from original given string
+   def create_rows
+       rows = []
+       given = @given.split("")
+    until given.length == 0
+       row = given.slice!(0..8)
+       rows << row
+    end
+      rows
    end
 
   #end
@@ -19,7 +29,7 @@ class Sudoku
   # Don't spend too much time on this method; flag someone from staff
   # if you are.
   def board
-
+    p @given.to_s
   end
 end
 
@@ -33,5 +43,6 @@ game = Sudoku.new(given,given_row)
 # Remember: this will just fill out what it can and not "guess"
 # game.solve!
 
-# puts game.board
-game.check_row(given_row)
+p game.board == "105802000090076405200400819019007306762083090000061050007600030430020501600308900"
+p game.check_row([0,0,3,5,0,1,0,9,4]) == [2, 6, 7, 8]
+p game.create_rows
