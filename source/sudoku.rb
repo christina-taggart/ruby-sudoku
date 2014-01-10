0fafcd1175c1e2eb3f62821d72c922aa68a5d656
Aditya Mahesh & John Olmsted <pair+amahesh0501.qsymmachus@devbootcamp.com>
2014-01-10 12:50:06 -0800

    Saves progress before lunch, adds get_row and get_column methods

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -4,21 +4,28 @@ class Sudoku
   def initialize(board_string)
     board = []
     board_string.split("").each_slice(9) {|row| board << row }
-    matrix_board = Matrix.rows(board)
-    # p board
-    # p "________"
-    # p matrix_board
-    # puts matrix_board[1,1]
+    @matrix_board = Matrix.rows(board)
+    p @matrix_board.to_a
   end
 
   def solve!
+    @possible_numbers = []
   end
+
 
   # Returns a string representing the current state of the board
   # Don't spend too much time on this method; flag someone from staff
   # if you are.
   def board
+      #p @board.each {|row| p row}
+  end
 
+  def get_row(square_index)
+    return @matrix_board.row(square_index[0]).to_a
+  end
+
+  def get_column(square_index)
+    return @matrix_board.column(square_index[1]).to_a
   end
 end
 
@@ -28,7 +35,6 @@ end
 board_string = File.readlines('sample.unsolved.txt').first.chomp
 game = Sudoku.new(board_string)
 
-p board_string
-
 game.solve!
 
+p game.get_column([0,2])
